8829b997e62faabc47ce97940f2efc6d6f225e80
dev4sinka <dev4.sinka@gmail.com>
2024-08-23 18:17:58 +0700

    a

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
-// Middleware to log and return the origin of the request
+// Middleware to log request info
 app.use((req, res, next) => {
-    const origin = req.headers.origin || 'unknown';
-    console.log(`Request Origin: ${origin}`);
-    res.send(`Request Origin: ${origin}`);
+    console.log('Request Info:');
+    console.log(`Method: ${req.method}`);
+    console.log(`URL: ${req.url}`);
+    console.log(`Headers: ${JSON.stringify(req.headers)}`);
+    console.log(`Body: ${JSON.stringify(req.body)}`);
+
+    // Return the same info in the response
+    res.json({
+        method: req.method,
+        url: req.url,
+        headers: req.headers,
+        body: req.body
+    });
 });
 
+// Example route
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
+const port = 3000;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
