fcb5268f7f5ae890d0c20e0ea2c58754a07b1b64
dev4sinka <dev4.sinka@gmail.com>
2024-08-23 18:15:00 +0700

    cors

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-// Middleware to log every request
+// Middleware to log and return the origin of the request
 app.use((req, res, next) => {
-    console.log(`Request: ${req.method} ${req.url}`);
-    next();
+    const origin = req.headers.origin || 'unknown';
+    console.log(`Request Origin: ${origin}`);
+    res.send(`Request Origin: ${origin}`);
 });
 
-// Define your routes here...
-
-// Start the server
-app.listen(8080, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
